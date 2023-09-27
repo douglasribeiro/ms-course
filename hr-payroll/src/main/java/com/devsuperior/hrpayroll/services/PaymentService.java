b5276ee946ee47791ed966258248c5f2324455f7
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2023-09-27 15:39:37 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -10,19 +10,10 @@ import com.devsuperior.hrpayroll.feignClients.WorkerFeignClient;
 @Service
 public class PaymentService {
 	
-//	@Value("${hr-worker.host}")
-//	private String workerHost;
-	
-	//@Autowired
-	//private RestTemplate restTemplate;
 	@Autowired
 	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-//		Map<String,String> uriVariables = new HashMap<>();
-//		uriVariables.put("id", ""+workerId);
-		
-//		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
 		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
